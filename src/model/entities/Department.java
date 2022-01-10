0367a0a495d8572286590fe12d79443692e4ea1a
Isaquesantos7 <Isaquesantos.second.1998@gmail.com>
2022-01-10 15:54:06 -0300

    Created entity Department

---
+++ src/model/entities/Department.java
@@ -0,0 +1,54 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Department {
+	private Integer id;
+	private String name;
+	
+	public Department() {
+		
+	}
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Id: " + this.getId());
+		sb.append(", Department: " + this.getName());
+		
+		return sb.toString();
+	}
+}
