b36953667fa6264cf01d0c68ee2f79f29daf2cb2
Isaquesantos7 <Isaquesantos.second.1998@gmail.com>
2022-01-11 08:17:12 -0300

    created sellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program{
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Department obj = new Department(1,"Books");
+		
+		SellerDao sellerDao = new DaoFactory().createSellerDao();
 		
 		Seller seller = new Seller(1, "Isaque santos", "isaquesantos.1998@gmail.com", sdf.parse("27/03/1999"), 2000.0, obj);
 		System.out.println(seller);
